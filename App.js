19af9a37eeca8a46b9e08ed82b154268446746e2
DishapBy <dishapby@gmail.com>
2022-05-18 16:48:44 +0300

    'fix'

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import ImagesContainer from "./components/imagesContainer/ImagesContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <ImagesContainer />
+    </div>
+  );
+}
+
+export default App;
